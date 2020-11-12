4499b5a179c02bfac39989e7b6adaae9d4d2d6e9
ashen-develops <ashenp.designs@gmail.com>
2020-11-12 02:02:17 -0600

    helmet and cors implemented

--- server.js
+++ server.js
@@ -2,39 +2,58 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const POKEDEX = require('./pokedex.json');
 
 console.log(process.env.API_TOKEN);
+const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
 
 const app = express();
 
 app.use(morgan('dev'));
-
-const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next){
 //   const bearerToken = req.get('Authorization').split(' ')[1];
+  const authToken = req.get('Authorization');
   const apiToken = process.env.API_TOKEN;
 
-  console.log('validate bearer token middleware');
+  // console.log('validate bearer token middleware');
 
-//   if(bearerToken !== apiToken){
-//     return res.status(401).json({error: 'Unauthorized request'});
-//   }
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({error: 'Unauthorized request'});
+  }
+  //   if(bearerToken !== apiToken){
+  //     return res.status(401).json({error: 'Unauthorized request'});
+  //   }
   //move to the next middleware
   next();
 });
 
-function handleGetTypes(req, res) {
+app.get('/types', function handleGetTypes(req, res) {
   res.json(validTypes);
-}
+});
 
-app.get('/types', handleGetTypes);
+app.get('/pokemon', function handleGetPokemon(req, res) {
+  let response = POKEDEX.pokemon;
 
-function handleGetPokemon(req, res) {
-  res.send('Hello, Pokemon!');
-}
-
-app.get('/pokemon', handleGetPokemon);
+  //filter pokemon by name if name query param
+  if(req.query.name){
+    response = response.filter(pokemon => 
+      //case insensitive searching
+      pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+    );
+  }
+  //filter pokemon by type if type query param
+  if(req.query.type){
+    response = response.filter(pokemon =>
+      pokemon.type.includes(req.query.type)
+    );
+  }
+  res.json(response);
+});
 
 const PORT = 8000;
 
